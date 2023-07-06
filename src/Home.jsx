6565ec69abe3ae2f734919368ebf379f37d0565f
V2NX <asim.mca22@cs.du.ac.in>
2023-07-06 16:32:49 +0530

    first commit

---
+++ src/Home.jsx
@@ -0,0 +1,49 @@
+import './App.css';
+
+function Home() {
+    return(
+        <>
+    <img class="img1"src="https://images.pexels.com/photos/106399/pexels-photo-106399.jpeg?cs=srgb&dl=pexels-binyamin-mellish-106399.jpg&fm=jpg" width="1210px" height="700px" alt="Description of the image"/>
+        <span class="quote1">Happy Home</span>
+    <h2>Top Picks</h2>
+        <div class="img2">
+            <img class="rounded float-start"src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYPkZVj1w9e_MNDSh97EIa5sDVLFBcgyoZ1A&usqp=CAU" width="250px" height="150px"/>
+            <p>Dwarka</p>
+        </div>
+        <div class="img3">
+            <img class="rounded mx-auto d-block" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSzM5aDv7z-RMheIQWMkh25OVIW73assZL8uw&usqp=CAU" width="250px" height="150px"/>
+            <p>Patel Nagar</p>
+        </div>
+        <div class="img4">
+            <img class="rounded float-end" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwRt7xpT1PREv7b8ITTyy15sqUnl883DUlTQ&usqp=CAU" width="250px" height="150px"/>
+            <p>Janak Puri</p>
+        </div>
+        <div class="img5">
+            <img class="rounded float-end" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwRt7xpT1PREv7b8ITTyy15sqUnl883DUlTQ&usqp=CAU" width="250px" height="150px"/>
+            <p>Maya Puri</p>
+        </div>
+        <div class="border border-end-0">
+            <p class="pyp">Post Your Property</p>
+            <button type="button" class="btn btn-info">Add</button>
+        </div>
+
+    <h2>Recently Viewed</h2>
+        <div class="img6">
+            <img class="rounded float-start"src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYPkZVj1w9e_MNDSh97EIa5sDVLFBcgyoZ1A&usqp=CAU" width="250px" height="150px"/>
+            <p>Dwarka</p>
+        </div>
+        <div class="img7">
+            <img class="rounded mx-auto d-block" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSzM5aDv7z-RMheIQWMkh25OVIW73assZL8uw&usqp=CAU" width="250px" height="150px"/>
+            <p>Patel Nagar</p>
+        </div>
+        <div class="img8">
+            <img class="rounded float-end" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwRt7xpT1PREv7b8ITTyy15sqUnl883DUlTQ&usqp=CAU" width="250px" height="150px"/>
+            <p>Janak Puri</p>
+        </div>
+        <div class="img9">
+            <img class="rounded float-end" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwRt7xpT1PREv7b8ITTyy15sqUnl883DUlTQ&usqp=CAU" width="250px" height="150px"/>
+            <p>Maya Puri</p>
+        </div>
+  </>
+    )
+}
